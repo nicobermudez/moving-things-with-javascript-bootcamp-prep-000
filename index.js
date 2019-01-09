3e1eb31adbdf6d49f7f6fbcec9a89c933de6b987
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-09 18:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ document.addEventListener('keydown', function(e) {
     moveDodgerLeft()
   }
 })
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
