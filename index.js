b7f6e3e846775a9c5544f8c19afc1b096737dd86
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-09 18:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
-dodger.style.backgroundColor = "#000000"
+dodger.style.backgroundColor = '#FF69B4'
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
